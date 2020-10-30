ef0a44c3a34301a9b27b6d473e6855fbe8a0aa1e
chimingg <68934510+chimingg@users.noreply.github.com>
2020-10-31 02:16:48 +0700

    Add files via upload

---
+++ week3-04.php
@@ -0,0 +1,33 @@
+<?php
+/*ID: 612110270
+Name: Zhiming zheng
+ */
+$sum=0;$score=[];
+$number=0;$i=0;
+while(true)
+{ 
+  echo"input score ".($i+1).": ";
+  $score[$i] = trim(fgets(STDIN));
+  if($score[$i]=='e')
+  {
+    break;
+  }
+  $sum+=$score[$i];
+  $i=$i+1;
+}
+$number=$i;
+$average=$sum/$number;
+echo"The average score for ";
+for($j=0;$j<=$number-1;$j++)
+{
+  echo $score[$j];
+  if($j==$number-2)
+  {
+    echo "and";
+  }
+  else if($j!=$number-2 && $j != $number-1)
+  {
+    echo ",";
+  }
+}
+echo "=".$average;
